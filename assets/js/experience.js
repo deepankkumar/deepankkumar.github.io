8657b5f9ef6dfccdfdd452aab0c586708241d91f
Deepank <deepanksingh1998@gmail.com>
2021-03-09 13:58:47 +0530

    Update experience.js

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -11,9 +11,9 @@ const exp = [
     desp: "<li>Predicted he recovery times of flood all over the US.</li> <li>based on a large number of hydro-meteorological and catchment characteristics specifically derived for this study.</li> <li>Deploying Statistical (Machine Learning) model to map flood recovery times across US.</li>",
   },
   {
-    title: "Summer Fellowship Research Programme-2020"
+    title: "Summer Fellowship Research Programme-2020",
     cardImage: "assets/images/experience-page/iitdelhi-logo.png",
-    place: "IIT Delhi" 
+    place: "IIT Delhi" ,
     time: "(July - Sept, 2020)",
     desp: "<li>developed an algorithm to observe the shape components of granular particles.</li><li>Used MATLAB for digital Image Processing to obtain complete geometry of the particle boundary, angularity and other shape parameters.</li><li>Implementation in field will save lot of time as compare to raditional methods.</li>",
   },
